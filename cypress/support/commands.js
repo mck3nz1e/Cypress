d63ed40aaa9416d22bd829f394dd4eac370179b0
Scott McKenzie <scottmckenzie09@googlemail.com>
2020-06-19 16:45:03 +0100

    Added custom command to navigate to form

---
+++ cypress/support/commands.js
@@ -0,0 +1,31 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+
+Cypress.Commands.add("navigateToForm", () => { 
+    cy.get('.category-cards > :nth-child(2)').click()
+    cy.get(':nth-child(2) > .element-list > .menu-list > #item-0').click()
+})
+
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
