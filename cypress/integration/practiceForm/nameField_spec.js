91b34609ad35c839e03d7633e0c33ed092eb6fd3
Scott McKenzie <scottmckenzie09@googlemail.com>
2020-06-19 16:44:22 +0100

    Added basic form test spec

---
+++ cypress/integration/practiceForm/nameField_spec.js
@@ -0,0 +1,29 @@
+describe("Practice Form - Name Field Tests", () => {
+
+    it("Verifys the User Name form label is correct", () => {
+        cy.visit("/");
+        cy.navigateToForm();
+
+        cy.get('#userName-label').should('have.text', 'Name')
+    });
+
+    it("Verifys the First Name form field shadow text is correct", () => {
+        cy.get('#firstName').should('have.attr', 'placeholder', 'First Name')
+    });
+
+    it("Verifys the First Name form field can accept input", () => {
+        cy.get("#firstName").type("Scott").should("have.value", "Scott");
+    });
+
+    it("Verifys the Last Name form field shadow text is correct", () => {
+        cy.get('#lastName').should('have.attr', 'placeholder', 'Last Name')
+    });
+
+    it("Verifys the Last Name form field can accept input", () => {
+        cy.get("#lastName").type("McKenzie").should("have.value", "McKenzie");
+    });
+
+    it("This is an example of a failed test", () => {
+        cy.get("#lastName").should("have.value", "FAIL");
+    });
+});
